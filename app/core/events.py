c3ab57b1eec70200de764fe4cd7280254dc9148e
parinaysethnewscorp <pseth.consultant@newscorp.com>
2025-05-14 12:38:17 +0530

    chore: Added mongo db connection on startup

--- app/core/events.py
+++ app/core/events.py
@@ -1,29 +1,32 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from api.routes import (general_routes, ai_routes, db_routes, health_check)
 import subprocess
-import logging
-import os
-import core.config as config
-
+import app.core.config as config
+from app.db.session import mongodb
+from app.core.logger_setup import setup_logger
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    
-
-    command = ["gcloud", "auth", "activate-service-account", f"--key-file={config.GOOGLE_APPLICATION_CREDENTIALS}"]
+    command = [
+        "gcloud",
+        "auth",
+        "activate-service-account",
+        f"--key-file={config.GOOGLE_APPLICATION_CREDENTIALS}",
+    ]
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-    
+    logger = setup_logger("lifespan_logger")
+    logger.info(f"Starting application in {config.ENV} environment")
+
     if process.returncode != 0:
-        logging.error(f"Google Cloud Authentication Error: {stderr.decode()}")
+        logger.error(f"Google Cloud Authentication Error: {stderr.decode()}")
         raise RuntimeError("Failed to authenticate with Google Cloud")
     else:
-        logging.info("Successfully authenticated with Google Cloud")
+        logger.info("Successfully authenticated with Google Cloud")
+
+    await mongodb.connect()
 
     yield
-    
-    
-    logging.info("Application is shutting down")
+
+    logger.info("Application is shutting down")
